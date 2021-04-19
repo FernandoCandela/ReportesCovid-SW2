afac093b3f5206b30afe4bb4de615bc18c4e6138
Andre Dupont <andredupontg@hotmail.com>
2021-04-18 22:43:11 -0500

    Dto Paciente

---
+++ DTO/DtoPaciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class DtoPaciente : DtoB
+    {
+        public int IdPaciente { get; set; } 
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int IN_Tipodoc { get; set; }
+        public string Numdoc { get; set; }
+        public int IN_TipoSeguro { get; set; }
+        public int IN_estadopaciente { get; set; }
+        public string UsuarioCreacionId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string UsuarioModificacionId { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public int IN_Estado { get; set; }
+        public string Credencial { get; set; }
+    }
+}
