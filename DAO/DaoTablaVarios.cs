e8609840704fcf5c2b942cc7370f8762860c4778
Andre Dupont <andredupontg@hotmail.com>
2021-06-22 17:07:25 -0500

    DaoTablaVarios -> Insert

--- DAO/DaoTablaVarios.cs
+++ DAO/DaoTablaVarios.cs
@@ -9,7 +9,7 @@ namespace DAO
 {
     public class DaoTablaVarios : DaoB
     {
-        public ClassResultV Usp_TablaVarios_Select(DtoB dtoBase)
+        public ClassResultV Usp_TablaVarios_SelectAll(DtoB dtoBase)
         {
             ClassResultV cr = new ClassResultV();
             var dto = (DtoTablaVarios)dtoBase;
@@ -25,7 +25,7 @@ namespace DAO
                     Value = (dto.EntidadTabla)
                 };
 
-                SqlDataReader reader = SqlHelper.ExecuteReader(ObjCn, CommandType.StoredProcedure, "Usp_TablaVarios_Select", pr);
+                SqlDataReader reader = SqlHelper.ExecuteReader(ObjCn, CommandType.StoredProcedure, "Usp_TablaVarios_SelectAll", pr);
 
                 cr.List = new List<DtoB>();
                 while (reader.Read())
@@ -54,6 +54,163 @@ namespace DAO
             }
             ObjCn.Close();
             return cr;
+        }
+        public DtoTablaVarios Usp_TablaVarios_Insert(DtoB dtoBase)
+        {
+            DtoTablaVarios dto = (DtoTablaVarios)dtoBase;
+            SqlParameter[] pr = new SqlParameter[6];
+            try
+            {
+                pr[0] = new SqlParameter("@IdTabVarios", SqlDbType.Int)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                pr[1] = new SqlParameter("@Valor", SqlDbType.VarChar, 200)
+                {
+                    Value = (V_ValidaPrNull(dto.Valor))
+                };
+                pr[2] = new SqlParameter("@Descripcion", SqlDbType.VarChar, 200)
+                {
+                    Value = (V_ValidaPrNull(dto.Descripcion))
+                };
+                pr[3] = new SqlParameter("@TipoAtributo", SqlDbType.VarChar, 200)
+                {
+                    Value = (V_ValidaPrNull(dto.TipoAtributo))
+                };
+                pr[4] = new SqlParameter("@EntidadTabla", SqlDbType.VarChar, 200)
+                {
+                    Value = (V_ValidaPrNull(dto.EntidadTabla))
+                };
+                pr[5] = new SqlParameter("@UsuarioCreacionId", SqlDbType.Int)
+                {
+                    Value = (V_ValidaPrNull(dto.UsuarioCreacionId))
+                };
+                SqlHelper.ExecuteNonQuery(ObjCn, CommandType.StoredProcedure, "Usp_TablaVarios_Insert", pr);
+                if (pr[0].Value != DBNull.Value)
+                {
+                    dto.IdTabVarios = Convert.ToInt32(pr[0].Value);
+                }
+                if (!String.IsNullOrEmpty(Convert.ToString(pr[9].Value)))
+                {
+                    dto.ErrorMsj = Convert.ToString(pr[9].Value);
+                    dto.LugarError = "Usp_TablaVarios_Insert";
+                }
+            }
+            catch (Exception ex)
+            {
+                dto.LugarError = ex.StackTrace;
+                dto.ErrorEx = ex.Message;
+                dto.ErrorMsj = "Error al insertar en TablaVarios";
+            }
+            ObjCn.Close();
+            return dto;
+        }
+        public ClassResultV Usp_Paciente_Update_ByIdPaciente(DtoB dtoBase)
+        {
+            DtoPaciente dto = (DtoPaciente)dtoBase;
+            ClassResultV cr = new ClassResultV();
+            SqlParameter[] pr = new SqlParameter[9];
+            try
+            {
+                pr[0] = new SqlParameter("@IdPaciente", SqlDbType.Int)
+                {
+                    Value = (dto.IdPaciente)
+                };
+                pr[1] = new SqlParameter("@Nombres", SqlDbType.VarChar, 100)
+                {
+                    Value = (V_ValidaPrNull(dto.Nombres))
+                };
+                pr[2] = new SqlParameter("@Apellidos", SqlDbType.VarChar, 100)
+                {
+                    Value = (V_ValidaPrNull(dto.Apellidos))
+                };
+                pr[3] = new SqlParameter("@IN_Tipodoc", SqlDbType.Int)
+                {
+                    Value = (V_ValidaPrNull(dto.IN_Tipodoc))
+                };
+                pr[4] = new SqlParameter("@Numdoc", SqlDbType.VarChar, 20)
+                {
+                    Value = (V_ValidaPrNull(dto.Numdoc))
+                };
+                pr[5] = new SqlParameter("@IN_TipoSeguro", SqlDbType.Int)
+                {
+                    Value = (V_ValidaPrNull(dto.IN_TipoSeguro))
+                };
+                pr[6] = new SqlParameter("@IN_EstadoPaciente", SqlDbType.Int)
+                {
+                    Value = (V_ValidaPrNull(dto.IN_EstadoPaciente))
+                };
+                pr[7] = new SqlParameter("@UsuarioModificacionId", SqlDbType.Int)
+                {
+                    Value = (V_ValidaPrNull(dto.UsuarioModificacionId))
+                };
+                pr[8] = new SqlParameter("@msj", SqlDbType.VarChar, 100)
+                {
+                    Direction = ParameterDirection.Output
+                };
+                SqlHelper.ExecuteNonQuery(ObjCn, CommandType.StoredProcedure, "Usp_Paciente_Update_ByIdPaciente", pr);
+                if (!String.IsNullOrEmpty(Convert.ToString(pr[8].Value)))
+                {
+                    cr.ErrorMsj = Convert.ToString(pr[8].Value);
+                    cr.LugarError = "Usp_Paciente_Update_ByIdPaciente";
+                }
+            }
+            catch (Exception ex)
+            {
+                cr.LugarError = ex.StackTrace;
+                cr.ErrorEx = ex.Message;
+                cr.ErrorMsj = "Error al actualizar los datos del paciente";
+            }
+            ObjCn.Close();
+            return cr;
+        }
+        public DtoPaciente Usp_Paciente_SelectOne(DtoB dtoBase)
+        {
+            DtoPaciente dto = (DtoPaciente)dtoBase;
+            SqlParameter[] pr = new SqlParameter[1];
+
+            try
+            {
+                pr[0] = new SqlParameter("@IdPaciente", SqlDbType.Int)
+                {
+                    Value = (dto.IdPaciente)
+                };
+
+                SqlDataReader reader = SqlHelper.ExecuteReader(ObjCn, CommandType.StoredProcedure, "Usp_Paciente_SelectOne", pr);
+
+                //cr.List = new List<DtoB>();
+                if (reader.Read())
+                {
+                    dto = new DtoPaciente
+                    {
+                        IdPaciente = GetValue("IdPaciente", reader).ValueInt32,
+                        Nombres = GetValue("Nombres", reader).ValueString,
+                        Apellidos = GetValue("Apellidos", reader).ValueString,
+                        IN_Tipodoc = GetValue("IN_Tipodoc", reader).ValueInt32,
+                        Numdoc = GetValue("Numdoc", reader).ValueString,
+                        IN_TipoSeguro = GetValue("IN_TipoSeguro", reader).ValueInt32,
+                        IN_EstadoPaciente = GetValue("IN_EstadoPaciente", reader).ValueInt32,
+                        UsuarioCreacionId = GetValue("UsuarioCreacionId", reader).ValueInt32,
+                        FechaCreacion = GetValue("FechaCreacion", reader).ValueDateTime,
+                        UsuarioModificacionId = GetValue("UsuarioModificacionId", reader).ValueInt32,
+                        FechaModificacion = GetValue("FechaModificacion", reader).ValueDateTime,
+                        IB_Estado = GetValue("IB_Estado", reader).ValueBool,
+                        Credencial = GetValue("Credencial", reader).ValueString,
+                        NombreTipodoc = GetValue("NombreTipodoc", reader).ValueString,
+                        NombreTipoSeguro = GetValue("NombreTipoSeguro", reader).ValueString,
+                        NombreEstadoPaciente = GetValue("NombreEstadoPaciente", reader).ValueString
+                    };
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                dto.LugarError = ex.StackTrace;
+                dto.ErrorEx = ex.Message;
+                dto.ErrorMsj = "Error en Usp_Paciente_SelectOne";
+            }
+            ObjCn.Close();
+            return dto;
         }
     }
 }
