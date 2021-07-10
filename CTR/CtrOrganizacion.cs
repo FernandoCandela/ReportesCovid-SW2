466e509d426cc4a46e37c0c13702c58ace352743
Andre Dupont <andredupontg@hotmail.com>
2021-07-10 09:19:03 -0500

    CtrOrganizacion ready

---
+++ CTR/CtrOrganizacion.cs
@@ -0,0 +1,13 @@
+﻿using DTO;
+using DAO;
+
+namespace CTR
+{
+    public class CtrOrganizacion
+    {
+        public DtoOrganizacion Usp_Organizacion_Insert(DtoB dtoBase) => new DaoOrganizacion().Usp_Organizacion_Insert(dtoBase);
+        public ClassResultV Usp_Organizacion_Select(DtoB dtoBase) => new DaoOrganizacion().Usp_Organizacion_SelectAll(dtoBase);
+        public ClassResultV Usp_Organizacion_Update_ByIdPaciente(DtoB dtoBase) => new DaoOrganizacion().Usp_Organizacion_Update_ByIdOrganizacion(dtoBase);
+        public DtoOrganizacion Usp_Organizacion_SelectOne(DtoB dtoBase) => new DaoOrganizacion().Usp_Organizacion_SelectOne(dtoBase);
+    }
+}
